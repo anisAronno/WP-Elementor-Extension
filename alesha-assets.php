99e7153f5bebbe5bf3989379713b30db39fc2e61
MD. Anichur Rahaman <38912435+anis3139@users.noreply.github.com>
2021-12-06 11:09:55 +0600

    Pro Product

--- alesha-assets.php
+++ alesha-assets.php
@@ -13,6 +13,45 @@ Domain Path: /languages/
 */
 
 
+/**
+ * Initialize the plugin tracker
+ *
+ * @return void
+ */
+
+/**
+ * Initialize the plugin tracker
+ *
+ * @return void
+ */
+function appsero_init_tracker_atl_extension() {
+
+    if ( ! class_exists( 'Appsero\Client' ) ) {
+      require_once __DIR__ . '/appsero/src/Client.php';
+    }
+
+    $client = new Appsero\Client( 'ea7e7b26-b1bb-4d8e-951b-c8699bb88103', 'atl-extension', __FILE__ );
+
+    // Active insights
+    $client->insights()->init();
+
+    // Active automatic updater
+    $client->updater();
+
+    // Active license page and checker
+    $args = array(
+        'type'       => 'options',
+        'menu_title' => 'atl-extension',
+        'page_title' => 'atl-extension Settings',
+        'menu_slug'  => 'atl_extension_settings',
+    );
+    $client->license()->add_settings_page( $args );
+
+}
+
+appsero_init_tracker_atl_extension();
+
+
 require_once( __DIR__ . '/atl-elementor-extension.php' );
 require_once( __DIR__ . '/social-icon.php' );
 
