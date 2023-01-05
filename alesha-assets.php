f91e41e6c8f955365a780ada4165b34c6f04b35b
Md. Anichur Rahaman <38912435+anis3139@users.noreply.github.com>
2023-01-05 12:55:37 +0600

    2.2.4

--- alesha-assets.php
+++ alesha-assets.php
@@ -4,7 +4,7 @@
 Plugin Name: Alesha Tech Assets
 Plugin URI: https://www.aleshatech.net
 Description: Alesha Tech Aseets
-Version: 2.2.3
+Version: 2.2.4
 Author: Alesha Tech
 Author URI: https://www.aleshatech.net
 License: GPLv2 or later
@@ -20,30 +20,39 @@ Domain Path: /languages/
  */
 require __DIR__ . '/vendor/autoload.php';
 
+
+
 /**
  * Initialize the plugin tracker
  *
  * @return void
  */
-function appsero_init_tracker_atl_extension() {
-
-    if ( ! class_exists( 'Appsero\Client' ) ) {
-      require_once __DIR__ . '/appsero/src/Client.php';
+function appsero_init_tracker_atl_extension()
+{
+    if (! class_exists('Appsero\Client')) {
+        require_once __DIR__ . '/appsero/src/Client.php';
     }
 
-    $client = new Appsero\Client( '5419e35b-c623-4c34-be39-cb6594e3289d', 'Alesha Tech Assets', __FILE__ );
+    $client = new Appsero\Client('5419e35b-c623-4c34-be39-cb6594e3289d', 'Alesha Tech Assets', __FILE__);
 
     // Active insights
-    $client->insights()->init();
+    $client->insights()->add_plugin_data()->init();
 
     // Active automatic updater
     $client->updater();
 
+    // Active license page and checker
+    $args = array(
+        'type'       => 'options',
+        'menu_title' => 'Alesha Tech Assets',
+        'page_title' => 'Alesha Tech Assets Settings',
+        'menu_slug'  => 'atl_extension_settings',
+    );
+    $client->license()->add_settings_page($args);
 }
 
 appsero_init_tracker_atl_extension();
 
-require_once( __DIR__ . '/atl-elementor-extension.php' );
-require_once( __DIR__ . '/social-icon.php' );
 
-
+require_once(__DIR__ . '/atl-elementor-extension.php');
+require_once(__DIR__ . '/social-icon.php');
